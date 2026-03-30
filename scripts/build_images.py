27b7047cdbacdb175bdbdcaa85bc82b8f86d9087
Ivan Beusan <100590096+ibeusan20@users.noreply.github.com>
2026-03-30 18:34:26 +0200

    Refactor image processing script for clarity

--- scripts/build_images.py
+++ scripts/build_images.py
@@ -60,7 +60,7 @@ def process_image(img: Image.Image) -> Image.Image:
     img = ImageEnhance.Sharpness(img).enhance(SHARPNESS_FACTOR)
     img = ImageEnhance.Contrast(img).enhance(CONTRAST_FACTOR)
     img = ImageOps.autocontrast(img, cutoff=AUTO_CONTRAST_CUTOFF)
-    img = img.point(lambda p: 255 if p > THRESHOLD else 0)
+    #img = img.point(lambda p: 255 if p > THRESHOLD else 0)
 
     return img
 
